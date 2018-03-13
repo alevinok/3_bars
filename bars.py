24c72f887451c4a364f8621adf2ae70eda6b1f92
alexey <alevinok@mail.com>
2018-03-13 19:08:54 +0300

    in work

--- bars.py
+++ bars.py
@@ -15,15 +15,21 @@ def create_list_seats_count_bar(file_data):
 
 
 def get_biggest_bar(file_data, list_seats_num):
+    big_bars_dict = dict()
     for atribut in file_data['features']:
         if max(list_seats_num) == int(atribut['properties']['Attributes']['SeatsCount']):
-            print(atribut['properties']['Attributes']['Name'], atribut['properties']['Attributes']['SeatsCount'])
+            big_bars_dict[atribut['properties']['Attributes']['Name']] \
+                = atribut['properties']['Attributes']['SeatsCount']
+    return big_bars_dict
 
 
 def get_smallest_bar(file_data, list_seats_num):
+    small_bars_dict = dict()
     for atribut in file_data['features']:
         if min(list_seats_num) == int(atribut['properties']['Attributes']['SeatsCount']):
-            print(atribut['properties']['Attributes']['Name'], atribut['properties']['Attributes']['SeatsCount'])
+            small_bars_dict[atribut['properties']['Attributes']['Name']] \
+                    = atribut['properties']['Attributes']['SeatsCount']
+    return small_bars_dict
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -34,8 +40,10 @@ if __name__ == '__main__':
     try:
         #path_to_file = sys.argv[1]
         path_to_file = 'bars.json'
-        get_biggest_bar(load_data(path_to_file), create_list_seats_count_bar(load_data(path_to_file)))
-        get_smallest_bar(load_data(path_to_file), create_list_seats_count_bar(load_data(path_to_file)))
+        print('Самый большой бар: %s ' %
+            get_biggest_bar(load_data(path_to_file), create_list_seats_count_bar(load_data(path_to_file))))
+        print('Самый маленький бар: %s ' %
+            get_smallest_bar(load_data(path_to_file), create_list_seats_count_bar(load_data(path_to_file))))
     except IndexError:
         print('Enter the path to the file.')
     except FileNotFoundError:
