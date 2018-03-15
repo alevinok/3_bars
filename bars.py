2ef76206c5c1879acffc9609d7370168057a33ef
alevinok <alevinok@gmail.com>
2018-03-15 07:26:29 +0300

    in work

--- bars.py
+++ bars.py
@@ -14,6 +14,15 @@ def create_list_seats_count_bar(file_data):
     return list_seats_count
 
 
+def create_dict_bars_coordinates(file_data):
+    bars_coordinates_dict = dict()
+    for atribut in file_data['features']:
+        bars_coordinates_dict[atribut['properties']['Attributes']['Name']] \
+            = atribut['geometry']['coordinates']
+    #return bars_coordinates_dict
+    print(bars_coordinates_dict)
+
+
 def get_biggest_bar(file_data, list_seats):
     big_bars_dict = dict()
     for atribut in file_data['features']:
@@ -46,13 +55,18 @@ if __name__ == '__main__':
     try:
         #path_to_file = sys.argv[1]
         path_to_file = 'bars.json'
-        parsed_file = load_data(path_to_file)
+        received_file = load_data(path_to_file)
         list_seats_bars = create_list_seats_count_bar(load_data(path_to_file))
-        #printing_dict_basr(get_biggest_bar(parsed_file, list_seats_bars))
+        #printing_dict_basr(get_biggest_bar(received_file, list_seats_bars))
         print('Самый большой бар: ')
-        printing_dict_basr(get_biggest_bar(parsed_file, list_seats_bars))
+        printing_dict_basr(get_biggest_bar(received_file, list_seats_bars))
         print('Самый маленький бар: ')
-        printing_dict_basr(get_smallest_bar(parsed_file, list_seats_bars))
+        printing_dict_basr(get_smallest_bar(received_file, list_seats_bars))
+        create_dict_bars_coordinates(load_data(path_to_file))
+        print('Введите Ваши координаты: ')
+        user_longitude = input('долгота: ')
+        user_latitude = input('широта: ')
+
     except IndexError:
         print('Enter the path to the file.')
     except FileNotFoundError:
